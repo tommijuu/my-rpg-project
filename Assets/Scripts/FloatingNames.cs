b8fa0d3194c4773bb69478502150891bef90627c
Tommi Juutilainen <juutilainen.tommi@gmail.com>
2022-03-08 13:10:19 +0200

    Renamed FloatingTexts to FloatingNames to be clear

--- Assets/Scripts/FloatingNames.cs
+++ Assets/Scripts/FloatingNames.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FloatingNames : MonoBehaviour
+public class FloatingTexts : MonoBehaviour
 {
     private GameObject parentObject;
 
